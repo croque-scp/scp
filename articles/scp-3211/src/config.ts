232a02e24d5b8c53f2ac92292f05638e4b2ab8e2
Ross Williams <rossjrw@gmail.com>
2021-02-02 05:36:40 +0000

    Configure encryption for each language
    Co-authored-by: RTa_technology <57354947+RTa-technology@users.noreply.github.com>

--- articles/scp-3211/src/config.ts
+++ articles/scp-3211/src/config.ts
@@ -15,23 +15,35 @@ export const anomalyNames = <const>[
 ]
 
 export type Lang = {
-  rot13: boolean
+  encrypt: [string, string, number][]
   fileUrl: string
 }
 
 export const langs: { [lang: string]: Lang } = {
   en: {
-    rot13: true,
+    encrypt: [
+      ["a", "z", 13],
+      ["A", "Z", 13],
+    ],
     fileUrl: "https://scp-wiki.wdfiles.com/local--files/scp-3211/"
   },
   zh: {
-    rot13: true,
+    encrypt: [
+      ["a", "z", 13],
+      ["A", "Z", 13],
+    ],
     fileUrl: "https://scp-wiki.wdfiles.com/local--files/scp-3211/"
     // fileUrl: "https://scp-wiki-cn.wdfiles.com/local--files/scp-3211/"
   },
   jp: {
-    rot13: true,
+    encrypt: [
+      ["a", "z", 13],
+      ["A", "Z", 13],
+      ["ぁ", "ゖ", 73],
+      ["ァ", "ヶ", 73],
+      ["一", "龠", 20217],
+    ],
     fileUrl: "https://scp-wiki.wdfiles.com/local--files/scp-3211/"
     // fileUrl: "http://scp-jp.wdfiles.com/local--files/scp-3211/"
-  }
+  },
 }
