28321d21c6a77aae7d20d952a58fbaa95741c75c
Ross Williams <rossjrw@gmail.com>
2024-08-23 01:20:47 +0100

    Process contradiction shorthand

--- source/scp-head/convert-statements.cjs
+++ source/scp-head/convert-statements.cjs
@@ -1,24 +1,50 @@
 #!/usr/bin/env node
 
-const inlineStatementPattern = /(?:\{.*?\})+/g;
-const blockStatementPattern = /(?:\{\{\{[\s\S]*?\}\}\}\s*)+/g;
+const patterns = {
+  inlineStatement: /(?:\{.*?\})+/g,
+  blockStatement: /(?:\{\{\{[\s\S]*?\}\}\}\n?)+/g,
+  contradiction: /\{=[A-Z0-9?^,]+\}/g,
+};
 const defaultChannel = "B";
 
-(async () => {
+if (require.main === module) main();
+
+async function main() {
   let source = "";
   for await (const chunk of process.stdin) source += chunk;
 
-  if (!source) {
-    console.error("No input. Usage: cat sourcefile | ./convert-assertions.cjs");
+  if (!source || !process.env.SCOUTFILE) {
+    if (!source) console.error("No input source");
+    if (!process.env.SCOUTFILE) console.error("No scout file specified");
+    console.error(
+      "Usage: (export SCOUTFILE=/scout.html; cat sourcefile | ./convert-statements.cjs)"
+    );
     process.exit(1);
   }
 
-  source = source
-    .replace(blockStatementPattern, replaceBlockStatement)
-    .replace(inlineStatementPattern, replaceInlineStatement);
+  source = replaceShorthand(source, process.env.SCOUTFILE);
 
   process.stdout.write(source);
-})();
+}
+
+module.exports = { replaceShorthand };
+
+/**
+ * Replaces shorthand syntax with wikitext.
+ *
+ * @param {string} source
+ * @param {string} scoutFile - URL of the scout iframe
+ * @returns {string}
+ */
+function replaceShorthand(source, scoutFile) {
+  return source
+    .replace(patterns.contradiction, (match) =>
+      replaceContradiction(match, scoutFile)
+    )
+    .replace(patterns.blockStatement, replaceBlockStatement)
+    .replace(patterns.inlineStatement, replaceInlineStatement)
+    .trim();
+}
 
 /**
  * Replaces inline statement shorthand with markup.
@@ -40,7 +66,7 @@ function replaceInlineStatement(match) {
         }
 
         return `[[span class="a_${assertion.stage}${
-          assertion.classes ? ` ${assertion.classes.trim()}` : ""
+          assertion.classes ? " " + assertion.classes.trim() : ""
         }"]]${assertion.text}[[/span]]`;
       }),
     "[[/span]]",
@@ -63,13 +89,47 @@ function replaceBlockStatement(match) {
       .flatMap((assertion) => {
         return [
           `[[div class="a_${assertion.stage}${
-            assertion.classes ? ` ${assertion.classes.trim()}` : ""
+            assertion.classes ? " " + assertion.classes.trim() : ""
           }"]]`,
           assertion.text.trim(),
           "[[/div]]",
         ];
       }),
-    "[[/div]]\n\n",
+    "[[/div]]\n",
+  ].join("\n");
+}
+
+/**
+ * Replaces contradiction shorthand with corresponding scout.
+ *
+ * @param {string} match
+ * @param {string} scoutFile - URL of the scout iframe
+ * @returns {string}
+ */
+function replaceContradiction(match, scoutFile) {
+  console.error("Replacing contradiction", match);
+  const conditionPattern = /[A-Z]?[0-9]+/;
+  const singleContradictionPattern = new RegExp(
+    conditionPattern.source + "(?:\\?" + conditionPattern.source + ")?\\^?"
+  );
+  const multiConditionPattern = new RegExp(
+    singleContradictionPattern.source +
+      "(?:," +
+      singleContradictionPattern.source +
+      ")*"
+  );
+  const contradictionShorthandPattern = new RegExp(
+    "^\\{=" + multiConditionPattern.source + "\\}$"
+  );
+  if (!contradictionShorthandPattern.test(match)) {
+    console.error({ match });
+    throw new Error("Invalid contradiction");
+  }
+  const contradictions = match.slice(2, -1);
+  return [
+    "[[embed]]",
+    `<iframe src="${scoutFile}?contradiction=${contradictions}" loading="lazy"></iframe>`,
+    "[[/embed]]",
   ].join("\n");
 }
 
@@ -86,7 +146,7 @@ function parseStatement(phrases) {
     .map((assertion) => {
       console.error("Parsing assertion", JSON.stringify(assertion));
       const match = assertion.match(
-        /(?:\s*([A-Z]?)([0-9]*)(?:( [ a-zA-Z0-9_-]+))?[:\n]\s*)?([\s\S]*?)$/
+        /(?:\s*([A-Z]?)([0-9]*)(?:( [ a-zA-Z0-9_-]+))?:\n?\s*)?([\s\S]*?)$/
       );
       const channel = String(match[1] || defaultChannel);
       const stage = Number(match[2]) || 10;
